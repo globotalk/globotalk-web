7777b7669e30a69bc72108d92abec8f8b4a70192
Bianca Rosa <me@biancarosa.com.br>
2018-04-28 15:55:04 -0300

    add response

--- src/MyAwesomeConversation.js
+++ src/MyAwesomeConversation.js
@@ -8,9 +8,17 @@ var messages = [{
   textColor: "white",
   avatar: 'https://www.seeklogo.net/wp-content/uploads/2015/09/google-plus-new-icon-logo.png',
   duration: 2000,
+},
+{
+  message:'Easy',
+  from: 'left',
+  backColor: '#3d83fa',
+  textColor: "white",
+  avatar: 'https://www.seeklogo.net/wp-content/uploads/2015/09/google-plus-new-icon-logo.png',
+  duration: 2000,
 }];
 
 const MyAwesomeConversation = () =>  
-  <Conversation height={300} messages={messages}/>
+  <Conversation height={300} messages={messages} turnOffLoop/>
 
 export default MyAwesomeConversation;
