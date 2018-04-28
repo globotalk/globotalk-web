c065492fd559a9e715992a5b0c7783f0222551bc
Bianca Rosa <me@biancarosa.com.br>
2018-04-28 15:50:12 -0300

    add conversation example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Demo from './Demo.js';
+import MyAwesomeConversation from './MyAwesomeConversation.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+ReactDOM.render(<MyAwesomeConversation />, document.getElementById('root'));
 registerServiceWorker();
